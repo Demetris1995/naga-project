1d91f9c6f39ed59608c4a1d6bb160bd43bfadf41
demetrism1995 <demetris_1995@hotmail.com>
2022-03-21 14:06:55 +0200

    added db folder

---
+++ web/sites/default/files/php/twig/6232f8c530b7c_page--front.html.twig_R_VUxVAGX0i545zG0O4MrLbC1/fzTJWhoz4RdsFQRPgbd3cAHTEB1SuvHR2K9HygrE0h8.php
@@ -0,0 +1,156 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/naga/templates/page--front.html.twig */
+class __TwigTemplate_2278588f6fc0c6d8d623f2921ab0e85088ff15b9e8d4fe86d075ad6e895dc985 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 46
+        echo "<div class=\"layout-container\">
+
+  <header role=\"banner\">
+    ";
+        // line 49
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "header", [], "any", false, false, true, 49), 49, $this->source), "html", null, true);
+        echo "
+  </header>
+
+  ";
+        // line 52
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "primary_menu", [], "any", false, false, true, 52), 52, $this->source), "html", null, true);
+        echo "
+  ";
+        // line 53
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "secondary_menu", [], "any", false, false, true, 53), 53, $this->source), "html", null, true);
+        echo "
+
+  ";
+        // line 55
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "breadcrumb", [], "any", false, false, true, 55), 55, $this->source), "html", null, true);
+        echo "
+
+  ";
+        // line 57
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "highlighted", [], "any", false, false, true, 57), 57, $this->source), "html", null, true);
+        echo "
+
+  ";
+        // line 59
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "help", [], "any", false, false, true, 59), 59, $this->source), "html", null, true);
+        echo "
+
+  <main role=\"main\">
+    <a id=\"main-content\" tabindex=\"-1\"></a>";
+        // line 63
+        echo "
+    ";
+        // line 64
+        if (twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "front", [], "any", false, false, true, 64)) {
+            // line 65
+            echo "      <div class=\"front\" class=\"content\">
+        ";
+            // line 66
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "front", [], "any", false, false, true, 66), 66, $this->source), "html", null, true);
+            echo "
+      </div>
+    ";
+        }
+        // line 69
+        echo " 
+ </main>
+
+  ";
+        // line 72
+        if (twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "footer", [], "any", false, false, true, 72)) {
+            // line 73
+            echo "    <footer role=\"contentinfo\">
+      ";
+            // line 74
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "footer", [], "any", false, false, true, 74), 74, $this->source), "html", null, true);
+            echo "
+    </footer>
+  ";
+        }
+        // line 77
+        echo "
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/naga/templates/page--front.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  105 => 77,  99 => 74,  96 => 73,  94 => 72,  89 => 69,  83 => 66,  80 => 65,  78 => 64,  75 => 63,  69 => 59,  64 => 57,  59 => 55,  54 => 53,  50 => 52,  44 => 49,  39 => 46,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/naga/templates/page--front.html.twig", "/var/www/html/web/themes/custom/naga/templates/page--front.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 64);
+        static $filters = array("escape" => 49);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
